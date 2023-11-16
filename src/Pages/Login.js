fec19b73dbdaa9df80d791df18d6c81ffb452520
Yunus Sezen <yunus.sezen@gmail.com>
2023-11-16 16:19:37 +0300

    .

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 
 export default function Login() {
 
-   const clientId = 'CLIENT_ID';
+   const clientId = '2054164ed4174c6aab877310ea9eee9f';
    const redirectUri = 'http://localhost:3000/callbackauth';
 
    const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;   
